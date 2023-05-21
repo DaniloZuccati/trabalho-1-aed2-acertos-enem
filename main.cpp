a6f24460d4336bea681f4440df7540c4826276c6
esqzerolol <68171967+esqzerolol@users.noreply.github.com>
2023-05-21 17:29:01 -0300

    COMMIT DOIDO

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include "Funcoes.h"
+
+int main (){
+    // Recebimento dos dados
+    vector<Aluno> dataSheet;
+    FILE *arquivoEntrada;
+    ArvoreNode* raiz;
+    arquivoEntrada = fopen("tratado.csv", "r");
+
+    cout << "COMECADO RECEBIMENTO/CRIACAO VETOR" << "\n";
+    
+    for (int i = 0; i < TAMANHOARQUIVO; i++){
+        dataSheet.push_back(CarregaDados(arquivoEntrada));
+    }    
+    fclose(arquivoEntrada);
+    cout << "FINALIZADO RECEBIMENTO/CRIACAO VETOR" << "\n";
+    // Finalizacao do recebimento dos dados
+
+    raiz = CriaArvore(dataSheet);
+
+    menu(raiz);
+
+    return 0;
+}
